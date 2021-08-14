799da44dfe6ddf120d0a5737aa4b66a91aa2e5f1
neuroplane <aleksey@boyarkin.com>
2021-08-14 16:17:17 +0300

    Kalinina commit

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ print(jsonObj)
 
 H = 640
 W = 384
+
 COL1LEFT = 20
 COL1RIGHT = W/2 - 20
+COL1CENTER = W/4
 COL2LEFT = W/2 + 20
 COL2RIGHT = W - 20
 CENTER = W/2
@@ -31,14 +33,26 @@ machsmall = ImageFont.truetype('fonts/mach.otf', 25)
 machbig = ImageFont.truetype('fonts/mach.otf', 140)
 unreal = ImageFont.truetype('fonts/unreal.ttf', 20)
 ubuntu = ImageFont.truetype('fonts/Ubuntu-M.ttf', 65)
-ubuntuс = ImageFont.truetype('fonts/ubuntuc.ttf', 65)
+ubuntuс = ImageFont.truetype('fonts/ubuntuc.ttf', 16)
 kroftsman = ImageFont.truetype('fonts/kroftsman.ttf', 150)
 kroftsmansm = ImageFont.truetype('fonts/kroftsman.ttf', 150)
-dfont = ubuntuс
+minecraftia = ImageFont.truetype('fonts/minecraftia.ttf', 16)
+mario = ImageFont.truetype('fonts/mario.ttf', 8)
+superstar = ImageFont.truetype('fonts/superstar.ttf', 16)
+pixeled = ImageFont.truetype('fonts/pixeled.ttf',10)
+dfont = mario
 greyFont = (230, 230, 230, 128)
 blackFont = (0, 0, 0)
-draw.text((CENTER, 10), "РАСПИСАНИЕ", font=dfont, fill=greyFont, anchor="mm")
-draw.text((COL1LEFT, 50), "COL1", font=dfont, fill=blackFont, anchor="mm")
-draw.text((COL1RIGHT, 50), "COL2", font=dfont, fill=greyFont, anchor="mm")
+LINESTART = 80
+LINEHEIGHT = 24
+draw.text((COL1CENTER, 30), "РАСПИСАНИЕ", font=dfont, fill=blackFont, anchor="mm")
+draw.text((COL1LEFT, LINESTART + LINEHEIGHT*0), "09:00 Русский язык", font=dfont, fill=greyFont, anchor="lm")
+draw.text((COL1LEFT, LINESTART + LINEHEIGHT*1), "10:00 Математика", font=dfont, fill=blackFont, anchor="lm")
+draw.text((COL1LEFT, LINESTART + LINEHEIGHT*2), "10:40 Природоведение", font=dfont, fill=blackFont, anchor="lm")
+draw.text((COL1LEFT, LINESTART + LINEHEIGHT*3), "13:00 Хоккей", font=dfont, fill=blackFont, anchor="lm")
+# draw.text((COL1RIGHT, LINESTART), "COL1R", font=dfont, fill=greyFont, anchor="rm")
+draw.text((COL2LEFT, LINESTART), "COL2L", font=dfont, fill=blackFont, anchor="lm")
+draw.text((COL2RIGHT, LINESTART), "COL2R", font=dfont, fill=greyFont, anchor="rm")
+draw.line((CENTER, LINESTART, CENTER, H), fill=greyFont, width=1)
 # This method will show image in any image viewer
 im.show()
