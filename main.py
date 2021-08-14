2c857c197775a0cba629f67be9e5f26173d67b64
neuroplane <aleksey@boyarkin.com>
2021-08-14 18:21:16 +0300

    Kalinina commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+import textwrap
 
 import jmespath
 import requests
@@ -9,11 +10,16 @@ from PIL import Image, ImageDraw, ImageFont, ImageOps
 from datetime import datetime, timezone, timedelta
 
 ##########################################################
+###### WIKIPEDIA
+wikiurl = "https://ru.wikipedia.org/w/api.php"
+querystring = {"action": "query", "format":"json", "titles":"электрон","prop":"extracts","utf8":"true","redirects":"1","exintro":"","explaintext":""}
 
-JGtoken = "pt0xh1e0nu1o8seqcgtxnlen2v9ye6htugzna55u"
-jsonObj = '[{"uid":"871f7d17-db4d-4863-8391-68113775d978","date":"December 14, 2021","name":"Lorie","country":"Tuvalu","lastname":"Hubbard"},{"uid":"ef444518-7d16-4732-9eaf-68abb6bc817e","date":"November 27, 2021","name":"Branch","country":"Bahrain","lastname":"Woodward"},{"uid":"b165968c-487d-4d29-bb34-6ec1ca21038b","date":"November 13, 2021","name":"Susan","country":"Lithuania","lastname":"Bright"},{"uid":"3613c0a6-f0f4-4104-8411-e1716239f502","date":"September 17, 2021","name":"Baxter","country":"Mauritania","lastname":"Matthews"},{"uid":"120fd700-9bd0-4a4e-8878-14fd83dcc7da","date":"October 8, 2021","name":"Adrian","country":"New Zealand","lastname":"Gomez"},{"uid":"128c4721-9dd4-4230-b52c-105c4ea089a1","date":"November 21, 2021","name":"Welch","country":"Sri Lanka","lastname":"Pollard"}]'
-print(jsonObj)
+payload = ""
+headers = {"cookie": "WMF-Last-Access=14-Aug-2021; WMF-Last-Access-Global=14-Aug-2021; GeoIP=RU%3ASE%3AVladikavkaz%3A43.03%3A44.67%3Av4"}
 
+response = requests.request("GET", wikiurl, data=payload, headers=headers, params=querystring).json()
+wotd = jmespath.search("query.pages.*.extract | [0]", response)
+print(wotd)
 H = 640
 W = 384
 
@@ -27,7 +33,7 @@ im = PIL.Image.new(mode="RGB", size=(384, 640), color=(255, 255, 255))
 draw = ImageDraw.Draw(im)
 ###############################################
 boston = ImageFont.truetype('fonts/nhlboston.ttf', 24)
-robotocond = ImageFont.truetype('fonts/robotocond.ttf', 16)
+robotocond = ImageFont.truetype('fonts/robotocond.ttf', 11)
 mach = ImageFont.truetype('fonts/mach.otf', 40)
 machsmall = ImageFont.truetype('fonts/mach.otf', 25)
 machbig = ImageFont.truetype('fonts/mach.otf', 140)
@@ -36,15 +42,15 @@ ubuntu = ImageFont.truetype('fonts/Ubuntu-M.ttf', 65)
 ubuntuс = ImageFont.truetype('fonts/ubuntuc.ttf', 16)
 kroftsman = ImageFont.truetype('fonts/kroftsman.ttf', 150)
 kroftsmansm = ImageFont.truetype('fonts/kroftsman.ttf', 150)
-minecraftia = ImageFont.truetype('fonts/minecraftia.ttf', 16)
+minecraftia = ImageFont.truetype('fonts/minecraftia.ttf', 8)
 mario = ImageFont.truetype('fonts/mario.ttf', 8)
 superstar = ImageFont.truetype('fonts/superstar.ttf', 16)
-pixeled = ImageFont.truetype('fonts/pixeled.ttf',10)
+pixeled = ImageFont.truetype('fonts/pixeled.ttf',6)
 dfont = mario
 greyFont = (230, 230, 230, 128)
 blackFont = (0, 0, 0)
-LINESTART = 80
-LINEHEIGHT = 24
+LINESTART = 50
+LINEHEIGHT = 20
 draw.text((COL1CENTER, 30), "РАСПИСАНИЕ", font=dfont, fill=blackFont, anchor="mm")
 draw.text((COL1LEFT, LINESTART + LINEHEIGHT*0), "09:00 Русский язык", font=dfont, fill=greyFont, anchor="lm")
 draw.text((COL1LEFT, LINESTART + LINEHEIGHT*1), "10:00 Математика", font=dfont, fill=blackFont, anchor="lm")
@@ -53,6 +59,11 @@ draw.text((COL1LEFT, LINESTART + LINEHEIGHT*3), "13:00 Хоккей", font=dfont
 # draw.text((COL1RIGHT, LINESTART), "COL1R", font=dfont, fill=greyFont, anchor="rm")
 draw.text((COL2LEFT, LINESTART), "COL2L", font=dfont, fill=blackFont, anchor="lm")
 draw.text((COL2RIGHT, LINESTART), "COL2R", font=dfont, fill=greyFont, anchor="rm")
-draw.line((CENTER, LINESTART, CENTER, H), fill=greyFont, width=1)
+index = 2
+wotdline = 16
+for line in textwrap.wrap(wotd, 65):
+    draw.text((COL1LEFT, H/2 + wotdline*index), line, font=robotocond, fill=blackFont, anchor="lm")
+    index = index + 1
+# draw.line((CENTER, LINESTART, CENTER, H), fill=greyFont, width=1)
 # This method will show image in any image viewer
 im.show()
