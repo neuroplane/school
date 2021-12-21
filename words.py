64e1284f981ffcc45f005e8f6c9d11acfd0b0e59
neuroplane <aleksey@boyarkin.com>
2021-12-21 16:20:50 +0300

    Kalinina commit

--- words.py
+++ words.py
@@ -12,26 +12,20 @@ from datetime import datetime, timezone, timedelta
 ###### WIKIPEDIA
 
 #########################################################
-H = 640
-W = 384
+H = 384
+W = 640
 
-COL1LEFT = 20
-COL1RIGHT = W/2 - 20
-COL1CENTER = W/4
-COL2LEFT = W/2 + 20
-COL2RIGHT = W - 20
-CENTER = W/2
-im = PIL.Image.new(mode="RGB", size=(384, 640), color=(255, 255, 255))
+im = PIL.Image.new(mode="RGB", size=(640, 384), color=(255, 255, 255))
 draw = ImageDraw.Draw(im)
 ###############################################
 boldfont52 = ImageFont.truetype('fonts/charisbold.ttf', 52)
 font52 = ImageFont.truetype('fonts/charis.ttf', 52)
 font44 = ImageFont.truetype('fonts/charis.ttf', 44)
 font32 = ImageFont.truetype('fonts/charis.ttf', 32)
+font24 = ImageFont.truetype('fonts/charis.ttf', 24)
+font16 = ImageFont.truetype('fonts/charis.ttf', 16)
 greyFont = (230, 230, 230, 128)
 blackFont = (0, 0, 0)
-
-import requests
 
 url = "https://x125.ru/api/school/randomword"
 
@@ -42,12 +36,14 @@ headers = {
 
 response = requests.request("POST", url, headers=headers).json()
 
-draw.text((W/2, H/2 - 130), response['word'], font=boldfont52, fill=blackFont, anchor="mm")
-draw.text((W/2, H/2), response['articulation'], font=font44, fill=blackFont, anchor="mm")
-draw.text((W/2, H/2 + 70), response['translation'], font=font32, fill=blackFont, anchor="mm")
+draw.text((W / 2, H / 2 - 120), response['word'], font=boldfont52, fill=blackFont, anchor="mm")
+draw.text((W / 2, H / 2 - 50), response['articulation'], font=font44, fill=blackFont, anchor="mm")
+draw.text((W / 2, H / 2 ), response['translation'], font=font24, fill=blackFont, anchor="mm")
+if response['example_en'] is not None:
+    draw.text((W / 2, H / 2 + 60), response['example_en'], font=font16, fill=blackFont, anchor="mm")
+if response['example_ru'] is not None:
+    draw.text((W / 2, H / 2 + 90), response['example_ru'], font=font16, fill=blackFont, anchor="mm")
 
 print(response)
 
-# draw.line((CENTER, LINESTART, CENTER, H), fill=greyFont, width=1)
-# This method will show image in any image viewer
 im.show()
